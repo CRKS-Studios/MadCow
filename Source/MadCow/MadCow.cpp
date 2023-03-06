5a00d96b85c175674fbae688527e7eceed4c5c1a
XDead27 <damageq12@gmail.com>
2023-03-06 02:50:38 +0100

    Initial commit

---
+++ Source/MadCow/MadCow.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MadCow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MadCow, "MadCow" );
---
+++ Source/MadCow/MadCowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MadCowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MADCOW_API AMadCowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
