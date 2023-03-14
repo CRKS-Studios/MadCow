86f5b938a94b23289bbccec34cc8e7f9b49f3a54
XDead27 <damageq12@gmail.com>
2023-03-14 03:15:25 +0100

    Same description as last commit

--- Source/MadCow/Private/SnapGridVolumeComponent.cpp
+++ Source/MadCow/Private/SnapGridVolumeComponent.cpp
@@ -3,3 +3,33 @@
 
 #include "SnapGridVolumeComponent.h"
 
+void USnapGridVolumeComponent::BeginPlay()
+{
+	this->interLayerDistance = (std::sqrt(3) / 2) * interCenterDistance;
+
+	this->XSize = interCenterDistance * this->numHexagonWidth;
+	this->YSize = interLayerDistance * this->numHexagonHeight;
+	this->ZLoc = this->GetUnscaledBoxExtent().Z / 2;
+
+	this->SetBoxExtent(FVector(XSize, YSize, this->GetUnscaledBoxExtent().Z));
+}
+
+TArray<FVector> USnapGridVolumeComponent::getHexCentersCoords()
+{
+	TArray<FVector> toReturn = TArray<FVector>();
+	
+	for (int i = 0; i < numHexagonWidth; i++) {
+		for (int j = 0; j < numHexagonHeight; j++) {
+			if (j % 2 == 0) {
+				toReturn.Add(FVector(i * this->interCenterDistance, j * interLayerDistance, ZLoc));
+			}
+			else {
+				if (i == numHexagonWidth - 1) continue;
+
+				toReturn.Add(FVector((i + 0.5) * this->interCenterDistance, j * interLayerDistance, ZLoc));
+			}
+		}
+	}
+
+	return toReturn;
+}
--- Source/MadCow/Public/SnapGridVolumeComponent.h
+++ Source/MadCow/Public/SnapGridVolumeComponent.h
@@ -14,4 +14,26 @@ class MADCOW_API USnapGridVolumeComponent : public UBoxComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Setup)
+	int64 numHexagonWidth = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Setup)
+	int64 numHexagonHeight = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Setup)
+	float interCenterDistance = 10.0;
+
+private:
+	float interLayerDistance = (std::sqrt(3) / 2) * interCenterDistance;
+
+	float XSize;
+	float YSize;
+	float ZLoc;
+
+public:
+	void BeginPlay();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	TArray<FVector> getHexCentersCoords();
 };
